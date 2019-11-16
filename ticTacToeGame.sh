511c774a6fea6c0d08aca841672b0ed6b8fdbf99
Admin <you@example.com>
2019-11-16 13:01:06 +0530

    player choose letter(x or 0) for playing

--- ticTacToeGame.sh
+++ ticTacToeGame.sh
@@ -1,28 +1,40 @@
-#!/bin/bash 
+#!/bin/bash -x 
 
 echo "******Wel-Come To TicTacToe Game******"
 
-p1=1
-p2=2
-p3=4
-p4=4
-p5=5
-p6=6
-p7=7
-p8=8
-p9=9
+position[0]=1
+position[1]=2
+position[2]=3
+position[3]=4
+position[4]=5
+position[5]=6
+position[6]=7
+position[7]=8
+position[8]=9
+
 
 function displayBoard()
 {
 	echo "|     |     |     |"
-	echo "|" $p1"   | "$p2"   |" $p3"   |"
+	echo "|" ${position[0]}"   | "${position[1]}"   |" ${position[2]}"   |"
 	echo "|_____|_____|_____|"
 	echo "|     |     |     |"
-	echo "|" $p4"   | "$p5"   |" $p6"   |"
+	echo "|" ${position[3]}"   | "${position[4]}"   |" ${position[5]}"   |"
 	echo "|_____|_____|_____|"
 	echo "|     |     |     |"
-	echo "|" $p7"   | "$p8"   |" $p9"   |"
+	echo "|" ${position[6]}"   | "${position[7]}"   |" ${position[8]}"   |"
 	echo "|     |     |     |"
 			
 }
-displayBoard
+function playGame()
+{
+	displayBoard
+	read -p "Player Choose letter(x or 0) for playing " playerLetter
+	if [ $playerLetter == x ] || [ $playerLetter == X ]
+	then
+		computerLetter=0
+	else
+		computerLetter=x
+	fi
+}
+playGame
