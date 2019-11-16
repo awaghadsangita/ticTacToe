ca5ebdb5f07266bf5d88a004ca638c91bbbd6d87
Admin <you@example.com>
2019-11-16 12:22:56 +0530

    display fresh board

--- ticTacToeGame.sh
+++ ticTacToeGame.sh
@@ -1,3 +1,28 @@
-#!/bin/bash -x
+#!/bin/bash 
 
 echo "******Wel-Come To TicTacToe Game******"
+
+p1=1
+p2=2
+p3=4
+p4=4
+p5=5
+p6=6
+p7=7
+p8=8
+p9=9
+
+function displayBoard()
+{
+	echo "|     |     |     |"
+	echo "|" $p1"   | "$p2"   |" $p3"   |"
+	echo "|_____|_____|_____|"
+	echo "|     |     |     |"
+	echo "|" $p4"   | "$p5"   |" $p6"   |"
+	echo "|_____|_____|_____|"
+	echo "|     |     |     |"
+	echo "|" $p7"   | "$p8"   |" $p9"   |"
+	echo "|     |     |     |"
+			
+}
+displayBoard
