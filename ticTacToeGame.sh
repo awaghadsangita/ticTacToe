af81037a8b1b8caff831a661441b9b6c9e90164a
Admin <you@example.com>
2019-11-18 10:20:26 +0530

    removed hardcoded values of displayBoard function

--- ticTacToeGame.sh
+++ ticTacToeGame.sh
@@ -2,28 +2,28 @@
 
 echo "******Wel-Come To TicTacToe Game******"
 
-position[0]=1
-position[1]=2
-position[2]=3
-position[3]=4
-position[4]=5
-position[5]=6
-position[6]=7
-position[7]=8
-position[8]=9
+declare -a position
 
+function initializedBoardArray()
+{
+	for(( i=0; i<9; i++ ))
+	do
+		position[$i]=$(($i+1))
+	done
+
+}
 
 function displayBoard()
 {
-	echo "|     |     |     |"
-	echo "|" ${position[0]}"   | "${position[1]}"   |" ${position[2]}"   |"
-	echo "|_____|_____|_____|"
-	echo "|     |     |     |"
-	echo "|" ${position[3]}"   | "${position[4]}"   |" ${position[5]}"   |"
-	echo "|_____|_____|_____|"
-	echo "|     |     |     |"
-	echo "|" ${position[6]}"   | "${position[7]}"   |" ${position[8]}"   |"
-	echo "|     |     |     |"
-			
+	i=0
+	for(( counter=0; counter<3; counter++ ))
+	do
+		echo "|     |     |     |"
+		echo "|" ${position[$((counter+i))]}"   | "${position[$((counter+i+1))]}"   |" ${position[$((counter+i+2))]}"   |"
+		echo "|     |     |     |"
+		i=$((counter+3))
+	done
+	
 }
+initializedBoardArray
 displayBoard
